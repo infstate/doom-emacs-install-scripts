4efbd6db93a4f1814e227ba84e41096ae2205696
Infinite State <165737308+infstate@users.noreply.github.com>
2024-12-31 20:45:05 -0800

    Update quickstart.sh

--- quickstart.sh
+++ quickstart.sh
@@ -1,7 +1,12 @@
 #!/bin/sh -e
+RC='\033[0m'  # From Linutil
+RED='\033[31m'
+YELLOW='\033[33m'
+CYAN='\033[36m'
+GREEN='\033[32m'
 git clone https://github.com/infstate/doom-emacs-install-scripts.git
 cd doom-emacs-install-scripts
 chmod +x doom-universal-linux.sh
-
-echo "Enviroment setup! Now just run any of the scripts! (e.g. ./doom-universal-linux.sh)"
-
+chmod +x doom-minimal-ubuntu.sh
+chmod +x doom-ubuntu.sh
+printf "%b\n" "${GREEN}Enviroment setup! Now just run any of the scripts! (e.g. ./doom-universal-linux.sh) ${RC}"
