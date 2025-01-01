e09e89fe64d47ef96212737207c2621dd16ab4b5
Infinite State <165737308+infstate@users.noreply.github.com>
2024-12-31 20:41:37 -0800

    Update quickstart.sh

--- quickstart.sh
+++ quickstart.sh
@@ -2,4 +2,6 @@
 git clone https://github.com/infstate/doom-emacs-install-scripts.git
 cd doom-emacs-install-scripts
 chmod +x doom-universal-linux.sh
-./doom-universal-linux.sh
+
+echo "Enviroment setup! Now just run any of the scripts! (e.g. ./doom-universal-linux.sh)"
+
